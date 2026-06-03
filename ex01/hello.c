0319f1b542e77de0c26118b6af3a6374a0ce64c2
galaxyze <galaxysun680@outlook.com>
2026-06-03 16:38:05 +0800

    完善 ex01 输入回显功能并更新 README

--- ex01/hello.c
+++ ex01/hello.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
+
 int main() {
-    printf("Hello, World!\n");
+    // 声明一个字符数组，用于存储键盘输入的内容
+    char input_content[100];
+
+    printf("请输入一些内容: ");
+    
+    // 使用 C 语言的输入函数获取键盘输入
+    scanf("%99s", input_content);
+
+    // 显示输出键盘输入的内容
+    printf("你输入的内容是: %s\n", input_content);
+
     return 0;
 }
